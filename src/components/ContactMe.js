0c993f72dacb0aa098dea8d58f3db4598fa54063
Isaac Diabagate <iadiabagate1s@semo.edu>
2021-02-16 18:18:15 -0600

    res march

--- src/components/ContactMe.js
+++ src/components/ContactMe.js
@@ -5,7 +5,7 @@ import Container from 'react-bootstrap/Container'
 import Button from 'react-bootstrap/Button'
 import gitl from '../images/github logo.png'
 import linked from '../images/linked in logo.png'
-import resume from '../images/IsaacDiabagate_Res_cover.docx (1).pdf'
+import resume from '../images/IsaacDiabagatemarch_Res.docx.pdf'
 
 export default function ContactMe() {
     return (
